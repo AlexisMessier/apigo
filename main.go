f4acc2a4b875df12014e18535043b7fd82ef4898
Alexis Messier <alexis.messier@hotmail.fr>
2018-03-26 16:14:53 +0200

    feat: ping

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Plop")
+	r := mux.NewRouter()
+	r.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("plop"))
+	})
+	http.ListenAndServe("127.0.0.1:8081", r)
 }
